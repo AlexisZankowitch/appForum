14398934e94ea56399c1cd1ae836a2eca86cb3b1
zankowitch alexis <zankowitch@gmail.com>
2015-08-05 23:37:08 +0200

    without chat pass  case

--- appForum/client/searchchatform.js
+++ appForum/client/searchchatform.js
@@ -1,15 +1,24 @@
 Template.searchChatForm.events({
     'click .joinChat' : function(e,t){
         e.preventDefault();
-        var chatRoomUsers = ChatRooms.find({
+        var chatRoom = ChatRooms.find({
             _id :  this._id
         }).fetch();
-        if(jQuery.inArray(Meteor.userId(),chatRoomUsers[0].users)>=0){
-            console.log('/chatroom/'+this._id);
+        console.log(chatRoom.password);
+        if(!chatRoom.password){
+            var data = {
+                chatRoomId : this._id,
+                userId : Meteor.userId()
+            };
+            Meteor.call('addUserToChat',data);
             Router.go('/chatroom/'+this._id);
         }else{
-            $("#hideidchatroom").val(this._id);
-            $('#joinchat-dialog').modal('toggle');
+            if(jQuery.inArray(Meteor.userId(),chatRoom[0].users)>=0){
+                Router.go('/chatroom/'+this._id);
+            }else{
+                $("#hideidchatroom").val(this._id);
+                $('#joinchat-dialog').modal('toggle');
+            }
         }
     }
 });
